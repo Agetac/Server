c2110d26e0ba7666e0b6ef9537049be6be8d4110
Twibit <clement.hardouin@gmail.com>
2012-03-14 14:22:17 +0100

    Correction changement constructeur Vehicule

--- Agetac-server/src/org/agetac/client/controller/VehiculeController.java
+++ Agetac-server/src/org/agetac/client/controller/VehiculeController.java
@@ -9,6 +9,7 @@ import org.agetac.common.model.impl.Groupe;
 import org.agetac.common.model.impl.Message;
 import org.agetac.common.model.impl.Position;
 import org.agetac.common.model.impl.Vehicule;
+import org.agetac.common.model.impl.Vehicule.CategorieVehicule;
 import org.agetac.common.model.impl.Vehicule.EtatVehicule;
 
 public class VehiculeController implements ActionListener {
@@ -27,12 +28,19 @@ public class VehiculeController implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand().equals("Ajouter un vehicule")) {
-			if (!(view.getID().equals("ID")) && !(view.getNom().equals("Nom")) && !(view.getPosition().equals("Position")) && !(view.getCaserne().equals("Caserne")) && !(view.getEtat().equals("Etat")) && !(view.getGroupe().equals("Groupe"))){
-			model.addVehicule(new Vehicule(view.getID(), view.getNom(), new Position(0, 0), view.getCaserne(), null, new Groupe(null, null, null)));
-			view.resetTxtFields();
+			if (!(view.getID().equals("ID")) && !(view.getNom().equals("Nom"))
+					&& !(view.getPosition().equals("Position"))
+					&& !(view.getCaserne().equals("Caserne"))
+					&& !(view.getEtat().equals("Etat"))
+					&& !(view.getGroupe().equals("Groupe"))) {
+
+				model.addVehicule(new Vehicule(view.getID(), view.getNom(),
+						new Position(0, 0), CategorieVehicule.FPT, view
+								.getCaserne(), Vehicule.EtatVehicule.ALERTE,
+						new Groupe(null, null, null), "0102"));
+				view.resetTxtFields();
 			}
-		}
-		else if (e.getActionCommand().equals("Supprimer un vehicule")) {
+		} else if (e.getActionCommand().equals("Supprimer un vehicule")) {
 			model.removeVehicule(view.getSelectedLine());
 		}
 
